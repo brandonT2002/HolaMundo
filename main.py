ad585e46111e11d4c5513c6646430ed84a51bfc2
brandonT2002 <3060571900304@ingenieria.usac.edu.gt>
2023-01-09 21:51:50 -0600

    enviando valores a las funciones

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def saludar(nombre):
-    print(f'Hola {nombre}')
+from operaciones import operaciones
+op = operaciones()
+num1 = int(input('Ingrese un número: '))
+num2 = int(input('Ingrese otro número: '))
 
-saludar('Joab Valle')
+print(f'La suma de {num1} + {num2} es: ',op.suma(num1,num2))
+print(f'La resta de {num1} - {num2} es: ',op.resta(num1,num2))
